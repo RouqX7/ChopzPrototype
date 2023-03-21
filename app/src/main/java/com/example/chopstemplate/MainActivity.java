874c1907213c0d2f13276f847ab1e0f4ff471861
RouqX7 <farouq2169@gmail.com>
2023-03-21 15:10:01 +0000

    Working....

--- app/src/main/java/com/example/chopstemplate/MainActivity.java
+++ app/src/main/java/com/example/chopstemplate/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.chopstemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageview5);
+        textView = findViewById(R.id.textview7);
+
+        imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                textView.animate().alpha(1f).setDuration(800);
+            }
+        });
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+
+            Intent intent = new Intent(MainActivity.this,MainMenu.class);
+            startActivity(intent);
+            finish();
+
+        },3000);
     }
 }
---
+++ app/src/main/java/com/example/chopstemplate/MainMenu.java
@@ -0,0 +1,19 @@
+package com.example.chopstemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainMenu extends AppCompatActivity {
+    Button signinemail,signinphone,signup;
+    ImageView bgi
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+    }
+}
